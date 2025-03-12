2a448bfc328e48bddcb5787a06739d9610e4e5de
elsa <nitikan.t@ku.th>
2025-03-12 21:23:48 +0700

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Sign from "./pages/Sign";
+
 import Profile from "./pages/Profile";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/sign" element={<Sign />} />
+       
         <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
