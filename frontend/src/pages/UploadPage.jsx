88c432206af1f0a15452dee9fe39202ff4c34cc6
phimphat1906 <phimlaphat.kh@ku.th>
2025-03-13 00:21:40 +0700

    uploadpage

---
+++ frontend/src/pages/UploadPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar2 from "../components/Navbar";
+import Background2 from "../components/Background";
+import Footer from "../components/Footer";
+
+function UploadPage() {
+  return (
+    <div>
+      <Navbar2 />
+      <Background2 />
+      <Footer />
+    </div>
+  );
+}
+
+export default UploadPage;
