278db17ed782885688806e540b5c62131f60eaab
phimphat1906 <phimlaphat.kh@ku.th>
2025-03-14 16:55:03 +0700

    Merge branch 'phimlaphat' of https://github.com/apinantianhaow/Projects into phimlaphat

--- frontend/src/components/ImageUploader.jsx
+++ frontend/src/components/ImageUploader.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import UploadIcon from "../assets/icons/Image.png"; 
 
 function ImageUploader() {
   const [selectedImage, setSelectedImage] = useState(null);
@@ -12,11 +13,18 @@ function ImageUploader() {
   };
 
   return (
+    <div className="flex flex-col items-center">
+      <h1
+        className="text-3xl font-regula text-[60px] mb-4 ml-[-60px] mt-[10px]"
+        style={{ fontFamily: "Kaisei HarunoUmi, sans-serif" }}
+      >
+        Create a new post
+      </h1>
+
     <div
-      className="relative w-[300px] h-[300px] flex justify-center items-center cursor-pointer"
+      className="relative w-[552px] h-[553px] flex justify-center items-center cursor-pointer mt-[50px] mr-[30px]"
       onClick={() => document.getElementById("fileInput").click()}
     >
-      {/* ถ้ามีรูปแสดงรูปเต็มกรอบ ถ้าไม่มีแสดงกรอบอัปโหลด */}
       {selectedImage ? (
         <img
           src={selectedImage}
@@ -24,12 +32,27 @@ function ImageUploader() {
           className="w-full h-full object-cover rounded-xl shadow-md"
         />
       ) : (
-        <div
-          className="flex flex-col justify-center items-center rounded-xl border-4 border-dashed border-gray-400 w-full h-full p-6"
-        >
-          <button className="px-4 py-2 bg-purple-600 text-white rounded-lg border-2 border-purple-700 pointer-events-none">
-            + Add photos
+        <div className="flex flex-col justify-center items-center rounded-xl border-8 border-dashed border-black w-full h-full p-6 ml-[-20px]">
+          <img src={UploadIcon} alt="Upload Icon" className="w-24 h-24 mb-4" />
+
+          <button
+            className="text-white font-semibold mt-[30px]"
+            style={{
+              width: "264px",
+              height: "64px",
+              backgroundColor: "#761FAC",
+              opacity: "0.9",
+              borderRadius: "20px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              border: "none",
+              cursor: "pointer",
+            }}
+          >
+            + Add Photos
           </button>
+
           <p className="mt-4 text-sm text-gray-500 text-center pointer-events-none">
             Add photos manually or just
             <br />
@@ -46,6 +69,7 @@ function ImageUploader() {
         onChange={handleImageUpload}
       />
     </div>
+  </div>
   );
 }
 
