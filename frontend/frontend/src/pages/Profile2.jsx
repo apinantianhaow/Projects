f0f437356381ab00391a2d87506ceca7428a3b0a
Akira Kato <akira78177@gmail.com>
2025-03-15 06:53:53 +0700

    Update project databases

---
+++ frontend/frontend/src/pages/Profile2.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import Topbar from "../components/Topbar";
+import Background2 from "../components/Background2";
+import Image from "../../src/assets/images/Dog1.png";
+import Footer from "../components/Footer";
+import Laurel from "../assets/images/Laurel2.jpg";
+import { IoBookOutline } from "react-icons/io5";
+import Postprofile from "../components/Postprofile";
+import Icontext from "../assets/icons/Message.png";
+
+function Profile() {
+    return (
+        <div>
+            <Topbar />
+            <Background2 />
+            <div className="w-full min-h-screen h-fit bg-cover bg-center flex flex-col ">
+                <div className="flex flex-row w-full h-[500px] items-center border-b-1 border-[#767676]">
+                     <img src={Image} alt="pf1" className="w-[335px] h-[335px]  ml-[120px]" /> 
+                     <div className=" ml-[60px]">
+                        <h1 className="text-[40px] font-bold ">
+                         killua zoldyck
+                        </h1>
+                        <h1 className="text-[34px] text-[#767676]">
+                         @killua zoldyck
+                        </h1>
+                     </div>
+                     <div className="flex flex-row ml-[100px] ">
+                        <img src={Laurel} alt="Laurel_L" className="w-[70px] h-[70px] object-contain" />
+                        <div>
+                        <h1 className="text-[30px] font-bold text-center ">
+                          295
+                        </h1>
+                        <h1 className="text-[24px] font-bold text-[#8D8D9E]">
+                          trades
+                        </h1>
+                     </div> 
+                        <img src={Laurel} alt="Laurel_R" className="w-[70px] h-[70px] object-contain scale-x-[-1]" />
+                     </div>
+                     <div className=" ml-[150px] ">
+                        <h1 className="text-[30px] font-bold text-center">
+                          981
+                        </h1>
+                        <h1 className="text-[24px] font-bold text-[#8D8D9E]">
+                          followers
+                        </h1>
+                     </div>
+                     <div className=" ml-[150px] ">
+                        <h1 className="text-[30px] font-bold text-center">
+                          2
+                        </h1>
+                        <h1 className="text-[24px] font-bold text-[#8D8D9E]">
+                          following
+                        </h1>
+                     </div>
+                     <div className=" ml-[130px] ">
+                     <img src={Icontext} alt="Message" className="w-[70px] h-[70px] object-contain" />
+                     </div>
+                </div>
+
+                <div className="justify-center ml-[130px]">
+                  <div className="bg-[#E5F0FF9C] w-[170px] h-[70px] mt-[70px] rounded-2xl flex flex-row items-center justify-center gap-[15px]">
+                  <IoBookOutline className="w-[35px] h-[35px] border-2 p-[5px] rounded-xl text-black"/>
+                  <h1 className="text-[25px] "> 
+                    POST
+                    </h1>
+                  </div>
+                </div>
+                <Postprofile/>
+            </div>
+            <Footer />
+        </div>
+    );
+}
+
+export default Profile;
--- frontend/src/components/CategorySelector.jsx
+++ frontend/src/components/CategorySelector.jsx
@@ -2,21 +2,28 @@ import { useState } from "react";
 
 function CategorySelector({ selectedCategory, setSelectedCategory, closeModal }) {
   const [tempCategory, setTempCategory] = useState(selectedCategory);
-  const categories = ["Electronics", "Clothing", "Books", "Home", "Plants", "Art&Handmade", "Collectibles", "Sports", "Kids", "Beauty", "Grocery"];
+  const categories = [
+    "Electronics", "Clothing", "Books", "Home", "Plants", "Art&Handmade", 
+    "Collectibles", "Sports", "Kids", "Beauty", "Grocery"
+  ];
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-white p-4 rounded-md w-96">
         <h3 className="font-bold text-lg mb-2">Select Category</h3>
         {categories.map((category, index) => (
-          <div key={index} className="p-2 cursor-pointer flex items-center" onClick={() => setTempCategory(category)}>
+          <div
+            key={index}
+            className="p-2 cursor-pointer flex items-center"
+            onClick={() => setTempCategory(category)} // เมื่อคลิก เลือกหมวดหมู่
+          >
             <input type="radio" checked={tempCategory === category} readOnly className="mr-2" />
             {category}
           </div>
         ))}
         <button 
           className="w-full bg-[#AC631F] hover:bg-opacity-80 text-white p-2 rounded-md mt-4" 
-          onClick={() => { setSelectedCategory(tempCategory); closeModal(); }}>
+          onClick={() => { setSelectedCategory(tempCategory); closeModal(); }}> {/* บันทึกหมวดหมู่ที่เลือก */}
           Save
         </button>
       </div>
