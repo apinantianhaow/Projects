5e7c371af4398f4e77bb9dfbf6962b7df2a1ef59
Apxnan <theiynhawxphinanth@gmail.com>
2025-03-26 09:58:37 +0700

    finish forget-reset password

--- frontend/src/pages/Forgetpassword.jsx
+++ frontend/src/pages/Forgetpassword.jsx
@@ -13,7 +13,7 @@ function Forgetpassword() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/check-email", {
+      const response = await fetch("http://localhost:5001/check-email", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email }),
